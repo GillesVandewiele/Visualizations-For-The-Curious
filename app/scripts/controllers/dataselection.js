9564109406bcdc1662ecde918debc1da47ee2961
givdwiel <givdwiel.vandewiele@ugent.be>
2015-03-20 11:47:51 +0100

    working on controllers

--- app/scripts/controllers/dataselection.js
+++ app/scripts/controllers/dataselection.js
@@ -10,4 +10,15 @@
 angular.module('dataVisualizationsApp.controllers')
   .controller('DataselectionCtrl', function ($scope) {
     $scope.gerard = 'zalig';
+    
+    $scope.url = "/data/files.json";
+    $scope.newFile = "";
+    $scope.files = []; 
+    console.log('received: ', $http.get($scope.url));
+    $scope.add = function(){
+    	$http.get($scope.url).then(function(response){
+    		$scope.newFile = response.data;//.queries.request.totalResults;
+            $scope.files.push($scope.newFile);
+    	});
+    };
   });
