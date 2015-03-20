ef958ed81ab856bd28d118cd172fae88f71911a7
Bruno Chevalier <bruno.chevalier@ugent.be>
2015-03-20 11:44:21 +0100

    voorbeeld $http dependency injection

--- app/scripts/controllers/dataselection.js
+++ app/scripts/controllers/dataselection.js
@@ -8,6 +8,18 @@
  * Controller of the dataVisualizationsApp
  */
 angular.module('dataVisualizationsApp.controllers')
-  .controller('DataselectionCtrl', function ($scope) {
+  .controller('DataselectionCtrl', ['$scope', '$http', function ($scope, $http) {
     $scope.gerard = 'zalig';
-  });
+
+    $http.get('data/KMI_daily.json').
+	  success(function(data, status, headers, config) {
+	    // this callback will be called asynchronously
+	    // when the response is available
+	    $scope.jsondata = data;
+	    console.log(data);
+	  }).
+	  error(function(data, status, headers, config) {
+	    // called asynchronously if an error occurs
+	    // or server returns response with an error status.
+	  });
+  }]);
