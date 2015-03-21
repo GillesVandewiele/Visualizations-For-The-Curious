2c521e3358b560a6eae5e2b15b4b908a6d5a3185
givdwiel <givdwiel.vandewiele@ugent.be>
2015-03-21 16:03:28 +0100

    support of multiple datasets now

--- app/scripts/controllers/dataselection.js
+++ app/scripts/controllers/dataselection.js
@@ -9,54 +9,102 @@ angular.module('dataVisualizationsApp.controllers')
   .controller('DataselectionCtrl', ['$scope', '$http', function ($scope, $http) {
 
   	/****** VARIABLES USED IN START SCREEN VIEW ******/
-	  	
-	  	$scope.files;
-	  	$scope.selectedFile;
-	  	
-	  	$scope.columns;
-	  	$scope.selectedLocationColumn;
-	  	$scope.selectedValueColumn;
-	  	$scope.selectedDateColumn;
+
+  		$scope.fileData = null; // The data stored in files.json on the server
 
 	  	$scope.aggregations = ["MEAN", "SUM", "MAX", "MIN", "COUNT"];
-	  	$scope.selectedAggregation = $scope.aggregations[0];
-
 	  	$scope.grouping = ["WEEKDAY", "WEEKS", "MONTH", "YEAR"];
-	  	$scope.selectedGrouping = $scope.grouping[0];
+
+	  	$scope.files; // Extra array for faster lookup in the datasets based on name
+	  	$scope.selectedFile;
+
+	  	$scope.datasets; // All datasets that were retrieved from the server
+	  	$scope.userDatasets = []; // The defined datasets by the user
+	  	$scope.currentDataset;
 
   	/*************************************************/
+
+  	// Private function to create a dataset (which has a lot of properties)
+  	var updateDataset = function(data, index){
+  		return {name:data[index].Name, columns:data[index].Columns, location:data[index].Columns[0],
+			value:data[index].Columns[1], date:data[index].Columns[data[index].Columns.length-1],
+			path: data[index].Path, aggregation:$scope.aggregations[0], grouping:$scope.grouping[0]};
+  	};
+
+  	var datasetTracker = function(dataset){
+  		return dataset.name + dataset.location + dataset.value + dataset.date + dataset.aggregation + dataset.grouping;
+  	}
 
     $http.get('data/files.json').
 	  success(function(data, status, headers, config) {  
 
+	  	// Save the data for later checks
+	  	$scope.fileData = data;
+
 	    // If the JSON-file was downloaded successfully, we populate all the dropdowns (files, columns) on the start screen
-    	$scope.files = []; 
-	    for(var file in data.Files) {
-	    	$scope.files.push(data.Files[file].Name)
+	    $scope.datasets = [];
+	    for(var i = 0; i < data.Files.length; i++){
+	    	$scope.datasets.push(updateDataset(data.Files, i));
 	    }
+	    $scope.files = []; 
+	    for(var file in data.Files) {
+	    	$scope.files.push(data.Files[file].Name);
+	    }
+	    $scope.currentDataset = updateDataset(data.Files, 0);
 	    $scope.selectedFile = $scope.files[0];
-	    $scope.columns = data.Files[0].Columns;
-	    $scope.selectedLocationColumn=$scope.columns[0];
-	    $scope.selectedValueColumn=$scope.columns[1];
-	    $scope.selectedDateColumn=$scope.columns[$scope.columns.length-1];
+	   	console.log("Current Dataset = ", $scope.currentDataset);
 
 	    // Based on the selection of dataset, we populate the other dropdowns
 	    $scope.populateDropdowns = function(){
-			var indexSelectedFile = $scope.files.indexOf($scope.selectedFile);
+			var indexSelectedDataset = $scope.files.indexOf($scope.selectedFile);
 			// If the index is equal to -1, 'Add file..' was selected
-			if(indexSelectedFile != -1){
-				$scope.columns = data.Files[indexSelectedFile].Columns;
-				$scope.selectedLocationColumn=$scope.columns[0];
-			    $scope.selectedValueColumn=$scope.columns[1];
-			    $scope.selectedDateColumn=$scope.columns[$scope.columns.length-1];
+			if(indexSelectedDataset != -1){
+				$scope.currentDataset = updateDataset(data.Files, indexSelectedDataset);
+				console.log("Current Dataset = ", $scope.currentDataset);
 			} else {
 				// TODO: Open a file browser so that the user can select a file to load
-				$scope.columns=[];
+				$scope.currentDataset.columns=[];
 			}
-			console.log($scope.columns)
-		}
+		};
 	  }).
 	  error(function(data, status, headers, config) {
 	    // TODO: show an error message on the homepage
 	  });
+
+	// This function is called when the user presses the 'Add' button and adds a dataset to a list to be downloaded later on.
+	$scope.addDataset = function(){
+		var contains = false;
+		for(var i = 0; i < $scope.userDatasets.length; i++){
+			if(datasetTracker($scope.userDatasets[i]) == datasetTracker($scope.currentDataset)){
+				contains = true;
+			}
+		}
+		if(!contains){
+			var copy = jQuery.extend(true, {}, $scope.currentDataset);
+			$scope.userDatasets.push(copy);
+		}
+	};
+
+	$scope.removeDataset = function(){
+
+	}
+
+  	// This function is called when the user presses the 'Finish' button and downloads all datasets from the list.
+	$scope.downloadData = function(){
+		if($scope.fileData != null){
+			for(var i = 0; i < $scope.userDatasets.length; i++){
+				$http.get($scope.userDatasets[i].path).
+			  		success(function(data, status, headers, config) { 
+			  			console.log("Data = ", data);
+			  			// TODO: validate all columns
+			  		}).
+			  		error(function(data, status, headers, config) {
+				    	// TODO: show an error message on the homepage
+					});
+			}
+		} else {
+			// TODO: show an error message on the homepage
+		}
+	};
+
   }]);
