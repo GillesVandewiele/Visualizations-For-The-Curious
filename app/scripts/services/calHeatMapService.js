4bb566ada2e8d8865a1e30e15d59546b5a67b876
Bruno Chevalier <bruno.chevalier@ugent.be>
2015-03-26 00:10:31 +0100

    Calendar visible now

--- app/scripts/services/calHeatMapService.js
+++ app/scripts/services/calHeatMapService.js
@@ -8,30 +8,27 @@
  * Provider in the calHeatMap module.
  */
 angular.module('d3')
-  .provider('calHeatMapService',['d3Service', function(d3Service){
-        d3Service.then(function(d3){
-            function createScript($document, callback) {
-                var scriptTag = $document.createElement('script');
-                scriptTag.type = "text/javascript";
-                scriptTag.async = true;
-                scriptTag.src = 'scripts/vendor/cal-heatmap.min.js';
-                scriptTag.onreadystatechange = function() {
-                    if (this.readyState == 'complete') {
-                        callback();
-                    }
+  .provider('calHeatMapService',function(){
+        function createScript($document, callback) {
+            var scriptTag = $document.createElement('script');
+            scriptTag.type = "text/javascript";
+            scriptTag.async = true;
+            scriptTag.src = 'scripts/vendor/cal-heatmap.min.js';
+            scriptTag.onreadystatechange = function() {
+                if (this.readyState == 'complete') {
+                    callback();
                 }
-                scriptTag.onload = callback;
-                $document.getElementsByTagName('body')[0].appendChild(scriptTag);
             }
- 
-            this.$get = ['$document','$q', '$window', '$rootScope',
-                function($document, $q, $window, $rootScope) {
-                    var deferred = $q.defer();
-                    createScript($document[0], function() {
-                        $rootScope.$apply(function()  { deferred.resolve($window.CalHeatMap) });;
-                    });
-                    return deferred.promise;
-                }]; 
-        });
-        
-  }]);
+            scriptTag.onload = callback;
+            $document.getElementsByTagName('body')[0].appendChild(scriptTag);
+        }
+
+        this.$get = ['$document','$q', '$window', '$rootScope',
+            function($document, $q, $window, $rootScope) {
+                var deferred = $q.defer();
+                createScript($document[0], function() {
+                    $rootScope.$apply(function()  { deferred.resolve($window.CalHeatMap) });;
+                });
+                return deferred.promise;
+            }]; 
+  });
