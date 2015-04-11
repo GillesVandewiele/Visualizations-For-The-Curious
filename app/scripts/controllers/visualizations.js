06a8b410042165d9f04b93628f65769a112931bf
serruys <karel.serruys@ugent.be>
2015-04-11 14:28:27 +0200

    extended dataservice with getActualData getTimesDict getLocationsDict

--- app/scripts/controllers/visualizations.js
+++ app/scripts/controllers/visualizations.js
@@ -8,11 +8,12 @@
  * Controller of the dataVisualizationsApp
  */
 angular.module('dataVisualizationsApp.controllers')
-  .controller('VisualizationsCtrl', ['$scope', 'dataService', function ($scope, dataService) {
+  .controller('VisualizationsCtrl', ['$scope', '$q', 'dataService', function ($scope, $q, dataService) {
 
-    function _initMap(){
-        
-        $scope.center = 
+    //this function initialises the leafletmap by centering the map
+    //and by selecting the right map on mapbox.
+    function _initMap(){        
+        $scope.center = {
             lat: 50.5,
             lng: 4.303,
             zoom: 8
@@ -34,4 +35,35 @@ angular.module('dataVisualizationsApp.controllers')
     }
 
     _initMap();
+
+
+    //getting data using promises
+    //this code is for testing porposes only,
+    //a watch should be used to check wether the data is updated or not
+    //after data is succesfully loaded into dataset,
+    //should the data be deleted in the service?
+    var dataset = [];
+    var dataPromise = [];
+    dataPromise[0] = dataService.getActualData(0);
+    dataPromise[1] = dataService.getActualData(1);
+
+    dataPromise[0]  
+        .then(function(data) {
+            console.log('Data loaded');
+            dataset[0] = data;
+            console.log(dataset[0]);
+
+        }, function(error) {
+            console.log('Failed to load data...', error);
+        });
+
+    dataPromise[1]  
+        .then(function(data) {
+            console.log('Data loaded');
+            dataset[1] = data;
+            console.log(dataset[1]);   
+        }, function(error) {
+            console.log('Failed to load data...', error);
+        });
+
   }]);
--- app/scripts/services/dataservice.js
+++ app/scripts/services/dataservice.js
@@ -8,26 +8,137 @@
  * Service in the dataVisualizationsApp.
  */
 angular.module('dataVisualizationsApp.services')
-  .service('dataService', function () {
-  	
-	var dataset = [];
+  .service('dataService', ['$q', '$http', function ($q, $http) {	
+	var userDatasets = [];
 	var count = 0;
 
+	var actualData = [];
+	var timesDict = [];
+	var locationsDict = [];
+
 	this.addOneDataset = function(d){
-		dataset[count] = d;
+		userDatasets[count] = d;
 		count++;	
 	};
 
 	this.addMultipleDatasets = function(ds){
 		for(var i=0;i<ds.length;i++){
-			dataset[count] = ds[i];
+			userDatasets[count] = ds[i];
 			count++;			
 		}
 	};
 
 	this.getDatasets = function(){
-		return dataset;	
-	};	
+	    var deferredGetDatasets = $q.defer();
+    	var promiseGetDatasets = deferredGetDatasets.promise;
 
-		
-  });
+    	if(userDatasets.length > 0){
+    		deferredGetDatasets.resolve(userDatasets);
+    	} else {
+    		deferredGetDatasets.reject('userDatasets not yet loaded');
+    	}
+
+		return deferredGetDatasets.promise;	
+	};
+
+	this.getDataset = function(index){
+	    var deferredGetDataset = $q.defer();
+    	var promiseGetDataset = deferredGetDataset.promise;
+
+    	if(userDatasets[index]){
+    		deferredGetDataset.resolve(userDatasets[index]);
+    	} else {
+    		deferredGetDataset.reject('userDatasets not yet loaded');
+    	}
+
+		return deferredGetDataset.promise;		
+	};
+
+	//function returning the actual data of the dataset with index index
+	this.getActualData = function(index){
+		var deferredGetActualData = $q.defer();
+		var promiseGetActualData = deferredGetActualData.promise;
+
+		var datasetInfoPromise = this.getDataset(index);
+
+		datasetInfoPromise
+			.then(function(data){
+				var deferredData = $q.defer();
+				var promiseData = deferredData.promise;
+				promiseData = $http.get(userDatasets[index].path);
+
+				promiseData
+					.then(function(data){
+						actualData[index] = data.data;
+						deferredData.resolve(actualData[index]);
+					});
+			});
+
+		if(actualData[index]){
+			deferredGetActualData.resolve(actualData[index]);
+		} else {
+			deferredGetActualData.reject('actualData not yet loaded');
+		}
+
+		return deferredGetActualData.promise;
+	}
+
+	//function returning the times dictionnary of the dataset with index index
+	this.getTimesDict = function(index){
+		var deferredGetTimesDict = $q.defer();
+		var promiseGetTimesDict = deferredGetTimesDict.promise;
+
+		var datasetInfoPromise = this.getDataset(index);
+
+		datasetInfoPromise
+			.then(function(data){
+				var deferredTimesDict = $q.defer();
+				var promiseTimesDict = deferredTimesDict.promise;
+				promiseTimesDict = $http.get(userDatasets[index].date.Dict);
+
+				promiseTimesDict
+					.then(function(data){
+						timesDict[index] = data.data;
+						deferredTimesDict.resolve(timesDict[index]);
+					});
+			});
+
+		if(timesDict[index]){
+			deferredGetTimesDict.resolve(timesDict[index]);
+		} else {
+			deferredGetTimesDict.reject('timesDict not yet loaded');
+		}
+
+		return deferredGetTimesDict.promise;
+	}
+
+	//function returning the locations dictionnary of the dataset with index index
+	this.getLocationsDict = function(index){
+		var deferredGetLocationsDict = $q.defer();
+		var promiseGetLocationsDict = deferredGetLocationsDict.promise;
+
+		var datasetInfoPromise = this.getDataset(index);
+
+		datasetInfoPromise
+			.then(function(data){
+				var deferredLocationsDict = $q.defer();
+				var promiseLocationsDict = deferredLocationsDict.promise;
+				promiseLocationsDict = $http.get(userDatasets[index].location.Dict);
+
+				promiseLocationsDict
+					.then(function(data){
+						locationsDict[index] = data.data;
+						deferredLocationsDict.resolve(locationsDict[index]);
+					});
+			});
+
+		if(locationsDict[index]){
+			deferredGetLocationsDict.resolve(locationsDict[index]);
+		} else {
+			deferredGetLocationsDict.reject('locationsDict not yet loaded');
+		}
+
+		return deferredGetLocationsDict.promise;
+	}
+
+  }]);
