1e8792a2e65526154e34c0557a4634f08dc9ba7e
Bruno Chevalier <bruno.chevalier@ugent.be>
2015-05-12 19:44:53 +0200

    index and karma

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -39,6 +39,7 @@ module.exports = function(config) {
       'bower_components/angular-chart.js/dist/angular-chart.js',
       'bower_components/d3/d3.js',
       'bower_components/angular-charts/dist/angular-charts.js',
+      'bower_components/d3-tip/index.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
